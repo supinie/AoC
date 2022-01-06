1d0c00fae0f184106afefb6f4798f852c3512d45
supinie <h44js2u2f@relay.firefox.com>
2022-01-06 22:33:31 +0000

    day 3

--- day_3.py
+++ day_3.py
@@ -15,6 +15,7 @@ def findMin(myList):
 
 url = "https://adventofcode.com/2021/day/3/input"
 gammaList = [[], [], [], [], [], [], [], [], [], [], [], []]
+co2List = [[], [], [], [], [], [], [], [], [], [], [], []]
 
 
 text = get_input(url)
@@ -28,6 +29,7 @@ instructions.pop()
 for i in range(0, len(instructions) - 1):
     for j in range(0, 12):
         gammaList[j].append(instructions[i][j])
+        co2List[j].append(instructions[i][j])
 
 gamma = findMax(gammaList)
 epsilon = findMin(gammaList)
@@ -38,3 +40,55 @@ epsilon = "".join(epsilon)
 result = int(gamma, 2)*int(epsilon, 2)
 
 print(result)
+
+
+unique = 0
+
+
+for i in range(0, len(gammaList)):
+    unique = int(gammaList[i].count(""))
+    if unique == 998:
+        break
+    counter = Counter(gammaList[i])
+    if counter["1"] == counter["0"] or counter["1"] > counter["0"]:
+        comparison = "1"
+    else:
+        comparison = "0"
+    for j in range(0, len(gammaList[i])):
+        if gammaList[i][j] != comparison:
+            for k in range(0, len(gammaList)):
+                gammaList[k][j] = ""
+
+unique = 0
+
+for i in range(0, len(co2List)):
+    unique = int(co2List[i].count(""))
+    if unique == 998:
+        break
+    counter = Counter(co2List[i])
+    if counter["0"] == counter["1"] or counter["0"] < counter["1"]:
+        comparison = "0"
+    else:
+        comparison = "1"
+    for j in range(0, len(co2List[i])):
+        if co2List[i][j] != comparison:
+            for k in range(0, len(co2List)):
+                co2List[k][j] = ""
+    
+        
+
+oxGen = ""
+co2 = ""
+
+for i in range(0, len(co2List)):
+    if "1" in set(gammaList[i]):
+        oxGen += "1"
+    else:
+        oxGen += "0"
+    if "1" in set(co2List[i]):
+        co2 += "1"
+    else:
+        co2 += "0"
+print(oxGen, co2)
+result2 = int(oxGen, 2)*int(co2, 2)
+print(result2)
